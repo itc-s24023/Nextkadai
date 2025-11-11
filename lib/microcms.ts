3464e46d0df92a267e63efb94e696c767e857f06
itc-s24023 <s24023@std.it-college.ac.jp>
2025-11-11 16:56:43 +0900

    api接続と topページ作成

---
+++ lib/microcms.ts
@@ -0,0 +1,17 @@
+import { createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || '',
+  apiKey: process.env.MICROCMS_API_KEY || '',
+});
+
+export type Area = {
+  id: string;
+  name: string;
+  description: string;
+  image?: {
+    url: string;
+  };
+  latitude: string;
+  longitude: string;
+};
